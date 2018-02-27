545be7509aab33ebeb13969c83353d44c7f0a18b
Владимир Бойко <E>
2018-02-27 14:37:13 +0200

    add foreach promise

--- index.js
+++ index.js
@@ -50,54 +50,58 @@ rp(opt.url).then(($) => {
   let arr = [];
   let cablesCrossArrayUnique = [];
   cables.forEach((arr1) => { arr = arr.concat(arr1); });
-  const promises = [];
-  for (let i = 0; i < arr.length; i += 1) {
-    const j = i;
-    const newOpt = {
-      url: opt.url + arr[i].link,
-      encoding: null,
-    };
-    promises.push(rp(newOpt.url).then(($) => {
-      const cablesVolteage = $('.UK_Tblb');
-      const cablesVolteageArray = [];
-      const cablesCross = $('.UK_Tbb');
-      const cablesCrossArray = [];
-      const variations = [];
-      
-      for (let i = 0; i < cablesVolteage.length; i += 1) {
-        let voltageWOKV = cablesVolteage[i].children[0].data.replace(/\sкВ/,'');
-        for (let k = 0; k < cablesCross.length; k += 1) {
-       
-          const testCross = cablesCross[k].children[0].data;
-          let test = testCross.match(regexp);
-          test = test[0].replace(/\s/g, '');
-          cablesCrossArray.push(test);
-          if(testCross.includes("-"+voltageWOKV)){
-            variations.push({
-              voltage: cablesVolteage[i].children[0].data,
-              cross: test
-            })
-          }
+  let action = (item,index) => {
+    return () => {
+      return new Promise((resolve) => {
+        const newOpt = {
+          url: opt.url + item.link,
+          encoding: null,
+        };
+        rp(newOpt.url).then(($) => {  
+          const cablesVolteage = $('.UK_Tblb');
+          const cablesVolteageArray = [];
+          const cablesCross = $('.UK_Tbb');
+          const cablesCrossArray = [];
+          const variations = [];
           
-        }
-        cablesCrossArrayUnique = cablesCrossArray.unique();
-        arr[j].cross = cablesCrossArrayUnique.join();
-        
-        cablesVolteageArray.push(cablesVolteage[i].children[0].data);
-        arr[j].voltage = cablesVolteageArray.join();
-        arr[j].variations = variations;
-      }
-      
-      return arr[j];
-    }));
-  }
-  return Promise.all(promises);
-})
-  .then((value) => {
-    value.forEach((t) => {
-      t.variations.forEach((e) =>{
-        console.log(t.title + " " + e.voltage + " " + e.cross);
+          for (let i = 0; i < cablesVolteage.length; i += 1) {
+            let voltageWOKV = cablesVolteage[i].children[0].data.replace(/\sкВ/,'');
+            for (let k = 0; k < cablesCross.length; k += 1) {
+              const testCross = cablesCross[k].children[0].data;
+              let test = testCross.match(regexp);
+              test = test[0].replace(/\s/g, '');
+              cablesCrossArray.push(test);
+              if(testCross.includes("-"+voltageWOKV)){
+                variations.push({
+                  voltage: cablesVolteage[i].children[0].data,
+                  cross: test
+                })
+              }
+              
+            }
+            cablesCrossArrayUnique = cablesCrossArray.unique();
+            item.cross = cablesCrossArrayUnique.join();
+            
+            cablesVolteageArray.push(cablesVolteage[i].children[0].data);
+            item.voltage = cablesVolteageArray.join();
+            item.variations = variations;
+          }
+          resolve(item);
+        });
+      })
+    }
+    }
+    let p = Promise.resolve();
+  arr.forEach(function(item,i){
+    p = p.then(action(item,i))
+  })
+}).then((value) => {
+    console.log(value)
+    /*value.forEach((t) => {
+    //  t.variations.forEach((e) =>{
+       // console.log(t.title + " " + e.voltage + " " + e.cross);
       }) 
         //fs.appendFile(file, ",variable,," + t.title + ",1,0,visible,,\"" + t.description + "\",,,taxable,,1,,0,0,,,,,1,,,,\"" + t.categorie + "\",,,,,,,,,,,,0,Напряжение,\"" + t.voltage + "\",1,1,Сечение,\"" + t.cross + "\",1,1\n")
     });
-  });
+    */
+  })
